dadda22122f59d7abc620357be23690720544aaf
ddderi <maxime830@live.fr>
2022-04-12 09:31:39 +0200

    commit tree 3ee37e357da07ed0e1770cee802e2bdd7368df26

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
 # Add  code here!
+
+
+def prime?(integer)
+if integer <= 0
+(-2..integer+1).none? {|div| integer % div == 0 }
+elsif integer >1 
+    (2..integer-1).none? {|div| integer % div == 0 }
+else false 
+end
+end
+
+p prime?(59)
